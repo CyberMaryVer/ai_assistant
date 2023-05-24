8743abeb2c767bb81c5225750e04d43bc6bcb504
ystukalov <ystukalov@akontech.ru>
2023-05-24 20:05:03 +0300

    endpoint company

--- fastapi_app/routes/keys/path.py
+++ fastapi_app/routes/keys/path.py
@@ -1,6 +1,7 @@
 import asyncpg
 from fastapi import Depends, APIRouter, Request
 from loguru import logger
+from starlette import status
 
 from .schemas import Key, KeyCreate
 from .servies import key_servise as servise
@@ -34,3 +35,27 @@ async def create_key(obj_in: KeyCreate,
     logger.debug(f"{company=}")
 
     return company
+
+
+@router.get("/{key_id}")
+async def get_key(key_id: int,
+                  db: asyncpg.Pool = Depends(get_db),
+                  ) -> Key:
+    # user_entries = get_entries_from_collection("users")
+    logger.debug("endpoint /get_key/ called")
+    logger.debug(f"{db=}")
+    key = await servise.get(db, key_id)
+
+    logger.debug(f"{key=}")
+
+    return key
+
+
+@router.delete("/{key_id}", status_code=status.HTTP_204_NO_CONTENT)
+async def delete_key(key_id: int,
+                     db: asyncpg.Pool = Depends(get_db),
+                     ):
+    logger.debug("endpoint /get_company/ called")
+    company = await servise.delete_key(db, key_id)
+
+    logger.debug(f"{company=}")
--- fastapi_app/routes/keys/servies.py
+++ fastapi_app/routes/keys/servies.py
@@ -1,27 +1,56 @@
 from typing import Generic, TypeVar, Type
+
+from fastapi import HTTPException
 from sqlalchemy.dialects import postgresql
 
 import asyncpg
 
-from sqlalchemy import select, insert, text
+from sqlalchemy import select, insert, text, delete
 
 from loguru import logger
+from starlette import status
 
 from fastapi_app.sql_tools import models
 from .schemas import KeyUpdate, KeyCreate, Key
+from ...sql_tools.models import engine
 
 
 class KeyService:
     def __init__(self, model: models.Keys):
         self.model = model
 
+    async def _compile(self, query) -> str:
+        compiled_query = query.compile(bind=engine,
+                                       compile_kwargs={"literal_binds": True}
+                                       )
+        logger.debug(str(compiled_query))
+
+        return str(compiled_query)
+
+    async def get(self, db: asyncpg.Pool, _id: int) -> list[models.Company]:
+        query = select(self.model).where(self.model.id == _id)
+
+        compiled_query = await self._compile(query)
+
+        async with db.acquire() as connection:
+            result = await connection.fetchrow(compiled_query)
+            logger.debug(f"{result=}")
+
+        if not result:
+            raise HTTPException(status.HTTP_404_NOT_FOUND, detail=f"id {_id} not found")
+
+        key = Key(**result)
+        logger.debug(f"{key}")
+
+        return key
+
     async def get_many(self, db: asyncpg.Pool) -> list[models.Keys]:
         query = select(self.model)
-        logger.debug(query)
+        compile_query = await self._compile(query)
 
         async with db.acquire() as connection:
             logger.debug(f"start connection")
-            result = await connection.fetch(str(query))
+            result = await connection.fetch(compile_query)
             logger.debug(f"{result=}")
 
         companies = [Key(**r) for r in result]
@@ -60,12 +89,19 @@ class KeyService:
             result = await connection.fetchrow(query, *values)
             logger.debug(f"{result=}")
 
-            # company_id = await connection.fetchval("SELECT LASTVAL()")
-            # logger.debug(f"{company_id=}")
-
         company = Key(**result)
 
         return company
+
+    async def delete_key(self, db: asyncpg.Pool, _id: int):
+        query = delete(self.model).where(self.model.id == _id).returning()
+        query = await self._compile(query)
+
+        async with db.acquire() as connection:
+            result = await connection.execute(query)
+            logger.debug(f"{result=}")
+
+        return result
 
 
 key_servise = KeyService(models.Keys)
